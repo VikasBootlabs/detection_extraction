3124e73f39b4f2a0f929fc02e93c421b31532cdd
PankajBootlabs <kumar.pankaj@bootlabstech.com>
2025-06-19 18:02:10 +0530

    adding

---
+++ routes/routes.py
@@ -0,0 +1,45 @@
+
+import os
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import StreamingResponse
+from services.dococr_extraction_services import GoogleDocOcr
+from services.signature_detecore_service import SignatureDetectorService
+
+import cv2
+import io
+from dotenv import load_dotenv
+load_dotenv()
+
+router = APIRouter()
+detector = SignatureDetectorService()
+extractore= GoogleDocOcr(
+    project_id=os.getenv("PROJECT_ID"),
+    processor_id=os.getenv("PROCESSOR_ID")
+)
+
+@router.post("/detect-signature")
+async def detect_signature(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    annotated_image, _ = detector.detect_signature(image_bytes)
+
+    _, encoded_image = cv2.imencode(".jpg", annotated_image)
+    image_stream = io.BytesIO(encoded_image.tobytes())
+
+    return StreamingResponse(image_stream, media_type="image/jpeg")
+
+
+
+@router.post("/extract-text")
+async def extract_text(file: UploadFile = File(...)):
+    extention=file.filename.split(".")[1]
+    extention_={
+        "pdf":"application/pdf",
+        "png":"image/png",
+        "jpg":"image/jpeg",
+        "jpeg":"image/jpeg"
+    }
+    mime=extention_[extention]
+    pdf_bytes = await file.read()
+
+    response = extractore.documentai_(pdf_bytes,mime)
+    return response
---
+++ services/dococr_extraction_services.py
@@ -0,0 +1,39 @@
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class GoogleDocOcr:
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']=os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    
+    def __init__(self,project_id,processor_id):
+        self.project_id=project_id
+        self.processor_id=processor_id
+        self.location='us'
+
+
+    def documentai_(self,path,mime_type):
+
+        pdf_bytes = path 
+
+        PROJECT_ID = self.project_id
+        LOCATION = self.location
+        PROCESSOR_ID = self.processor_id
+        MIME_TYPE = mime_type
+        docai_client = documentai.DocumentProcessorServiceClient(
+            client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
+        )
+
+        RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
+
+        raw_document = documentai.RawDocument(content=pdf_bytes, mime_type=MIME_TYPE)
+
+        request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
+        result = docai_client.process_document(request=request)
+
+        document_object = result.document
+        
+        return{"status_code":200,"message":" extract using documentai","data":document_object.text}
